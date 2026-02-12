ed21e12b96623c9c82d1c3802ec77e1302a29029
Aereises <ruthmetayer0@gmail.com>
2026-02-12 22:13:47 +0100

    fix: deplacement de certain fichier

---
+++ src/main/java/appli/StartApplication.java
@@ -0,0 +1,35 @@
+package appli;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class    StartApplication extends Application {
+    public static Stage mainStage;
+    @Override
+    public void start(Stage stage) throws IOException {
+        mainStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("accueil/loginView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        mainStage.setTitle("Connexion");
+        mainStage.setScene(scene);
+        mainStage.show();
+    }
+    public static void changeScene(String nomDuFichierFxml, String namePage) throws IOException {
+        FXMLLoader fxmlLoader = new
+                FXMLLoader(StartApplication.class.getResource(nomDuFichierFxml + "View.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.setUserData(fxmlLoader.getController());
+        mainStage.setScene(scene);
+        mainStage.setTitle(namePage);
+    }
+    public static Object getControllerFromStage(){
+        return mainStage.getScene().getUserData();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
